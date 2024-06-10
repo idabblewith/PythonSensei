9a135b64e9f9e034d10d8248121e5bac1b2a9c6a
idabblewith <109533655+idabblewith@users.noreply.github.com>
2024-06-10 11:54:08 +0800

    chore: day 58

--- tools/days/day_057/main.py
+++ tools/days/day_057/main.py
@@ -3,21 +3,18 @@ from days.day_057.files.helpers import *
 
 def day_057():
     title("FLASK: BASIC BLOG")
-
-    res = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
-    posts_data = res.json()
-
-    post_objects = []
-    for post in posts_data:
-        print(f"{post}\n")
-        post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
-        post_objects.append(post_obj)
-
     template_folder_path = os.path.join(os.path.dirname(__file__), "files", "templates")
     static_folder_path = os.path.join(os.path.dirname(__file__), "files", "static")
     app = Flask(
         __name__, template_folder=template_folder_path, static_folder=static_folder_path
     )
+    res = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
+    posts_data = res.json()
+
+    post_objects = []
+    for post in posts_data:
+        post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
+        post_objects.append(post_obj)
 
     @app.route("/")
     def get_all_posts():
--- tools/days/day_058/main.py
+++ tools/days/day_058/main.py
@@ -1,4 +1,17 @@
 from days.day_058.files.helpers import *
 
+
 def day_058():
-	title("FLASK: TINDOG")
+    title("FLASK: TINDOG")
+    template_folder_path = os.path.join(os.path.dirname(__file__), "files", "templates")
+    static_folder_path = os.path.join(os.path.dirname(__file__), "files", "static")
+    app = Flask(
+        __name__, template_folder=template_folder_path, static_folder=static_folder_path
+    )
+
+    @app.route("/")
+    def home():
+        return render_template("index.html")
+
+    if __name__ == "days.day_058.main":
+        app.run(debug=True, use_reloader=False)
