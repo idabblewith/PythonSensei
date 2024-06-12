f3567536f551e46a1b660d99b50371061eff5405
idabblewith <109533655+idabblewith@users.noreply.github.com>
2024-06-13 06:39:18 +0800

    chore: day 61

--- tools/days/day_061/files/helpers.py
+++ tools/days/day_061/files/helpers.py
@@ -1,1 +1,13 @@
-from misc import nls, nli, title, cls, bcolors
+from misc import nls, nli, title, cls, bcolors, os
+import requests
+from flask import Flask, render_template, request
+import smtplib
+from dotenv import load_dotenv
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import (
+    DataRequired,
+    Email,
+    Length,
+)  # pip install email-validator
+from flask_bootstrap import Bootstrap5  # pip install bootstrap-flask
--- tools/days/day_061/main.py
+++ tools/days/day_061/main.py
@@ -1,4 +1,39 @@
 from days.day_061.files.helpers import *
 
+
 def day_061():
-	title("FLASK: ADVANCED FORMS")
+    title("FLASK: ADVANCED FORMS")
+    load_dotenv()
+    template_folder_path = os.path.join(os.path.dirname(__file__), "files", "templates")
+    static_folder_path = os.path.join(os.path.dirname(__file__), "files", "static")
+    app = Flask(
+        __name__, template_folder=template_folder_path, static_folder=static_folder_path
+    )
+
+    app.secret_key = os.getenv("SOME_SECRET_KEY_FOR_FLASK")
+    bootstrap = Bootstrap5(app)
+
+    class LoginForm(FlaskForm):
+        email = StringField("Email", validators=[DataRequired()])
+        password = PasswordField("Password", validators=[DataRequired()])
+        submit = SubmitField(label="Log In")
+
+    @app.route("/")
+    def home():
+        return render_template("index.html")
+
+    @app.route("/login", methods=["GET", "POST"])
+    def login():
+        login_form = LoginForm()
+        if login_form.validate_on_submit():
+            if (
+                login_form.email.data == "admin@email.com"
+                and login_form.password.data == "12345678"
+            ):
+                return render_template("success.html")
+            else:
+                return render_template("denied.html")
+        return render_template("login.html", form=login_form)
+
+    if __name__ == "days.day_061.main":
+        app.run(debug=True, use_reloader=False)
