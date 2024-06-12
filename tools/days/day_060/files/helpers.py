e917de54a80132d1f3fcd2970f77afc21beb3496
idabblewith <109533655+idabblewith@users.noreply.github.com>
2024-06-13 06:20:13 +0800

    chore: day 60

--- tools/days/day_060/files/helpers.py
+++ tools/days/day_060/files/helpers.py
@@ -1,1 +1,5 @@
-from misc import nls, nli, title, cls, bcolors
+from misc import nls, nli, title, cls, bcolors, os
+import requests
+from flask import Flask, render_template, request
+import smtplib
+from dotenv import load_dotenv
--- tools/days/day_060/main.py
+++ tools/days/day_060/main.py
@@ -1,4 +1,51 @@
 from days.day_060.files.helpers import *
 
+
 def day_060():
-	title("FLASK: FORMS AND REQUESTS")
+    title("FLASK: FORMS AND REQUESTS")
+    template_folder_path = os.path.join(os.path.dirname(__file__), "files", "templates")
+    static_folder_path = os.path.join(os.path.dirname(__file__), "files", "static")
+    app = Flask(
+        __name__, template_folder=template_folder_path, static_folder=static_folder_path
+    )
+
+    load_dotenv()
+    SENDER_EMAIL = os.getenv("SENDER_EMAIL")
+    GOOGLE_APP_PASSWORD = os.getenv("GOOGLE_APP_PASSWORD")
+    RECEIVER_EMAIL = os.getenv("RECEIVER_EMAIL")
+
+    posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+
+    def send_email(name, email, phone, message):
+        email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(SENDER_EMAIL, GOOGLE_APP_PASSWORD)
+            connection.sendmail(SENDER_EMAIL, RECEIVER_EMAIL, email_message)
+
+    @app.route("/")
+    def get_all_posts():
+        return render_template("index.html", all_posts=posts)
+
+    @app.route("/about")
+    def about():
+        return render_template("about.html")
+
+    @app.route("/contact", methods=["GET", "POST"])
+    def contact():
+        if request.method == "POST":
+            data = request.form
+            send_email(data["name"], data["email"], data["phone"], data["message"])
+            return render_template("contact.html", msg_sent=True)
+        return render_template("contact.html", msg_sent=False)
+
+    @app.route("/post/<int:index>")
+    def show_post(index):
+        requested_post = None
+        for blog_post in posts:
+            if blog_post["id"] == index:
+                requested_post = blog_post
+        return render_template("post.html", post=requested_post)
+
+    if __name__ == "days.day_060.main":
+        app.run(debug=True, use_reloader=False)
