48b65a4e5bb5c4e764fe818079ed37370a296a4b
idabblewith <109533655+idabblewith@users.noreply.github.com>
2024-06-13 08:11:03 +0800

    chore: days 63-64

--- tools/days/day_063/files/helpers.py
+++ tools/days/day_063/files/helpers.py
@@ -1,1 +1,7 @@
-from misc import nls, nli, title, cls, bcolors
+from misc import nls, nli, title, cls, bcolors, os
+from flask import Flask, render_template, request, redirect, url_for
+from dotenv import load_dotenv
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
--- tools/days/day_064/files/helpers.py
+++ tools/days/day_064/files/helpers.py
@@ -1,1 +1,11 @@
-from misc import nls, nli, title, cls, bcolors
+from misc import nls, nli, title, cls, bcolors, os
+from dotenv import load_dotenv
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import requests
